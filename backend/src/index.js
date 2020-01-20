e89a11c3261599cb85f7d39330f282f9fa99c76d
unknown <ionverseorg@gmail.com>
2020-01-20 01:18:31 -0300

    Aplicação web utilizando react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +10,7 @@ mongoose.connect('mongodb+srv://matheus:matheus@cluster0-bwebc.mongodb.net/week1
     useUnifiedTopology: true,
 });
 
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {  useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import Devitem from './components/Devitem';
+import Devform from './components/Devform';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que o componente pai passa para o componente filho.
@@ -9,33 +15,36 @@ import './App.css';
 
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+ async function handleAddDev(data) {
+  const response = await api.post('/devs', data);
+
+  setDevs([...devs, response.data]);
+ }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group"></div>
-
-
-          <div class="input-block">
-            <label htmlFor="username_github">Usuário</label>
-            <input name="github_username" id="username_github" required/>
-          </div>
-        </form>
+        <Devform  onSubmit={handleAddDev} />
       </aside>
       <main>
-
+        <ul>
+          {devs.map(dev => (
+            <Devitem key={dev._id} dev={dev} />
+          ))}
+        </ul>
       </main>
     </div>
   );
